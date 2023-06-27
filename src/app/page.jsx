527f374501a9989b285f78448453683b3480a027
Emran Hossain <emranhossain0335@gmail.com>
2023-06-27 17:36:16 +0600

    NextJs web app

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className={styles.container}>
       <div className={styles.item}>
         <h1 className={styles.title}>
-          Better design for your digital products
+          Better Design for Your Digital Products
         </h1>
         <p className={styles.desc}>
           Turning your idea into reality. We bring together the teams from the
